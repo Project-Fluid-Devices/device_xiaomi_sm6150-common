845319ded43419da491b153bf3041fcad1977a58
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 11:15:11 +0800

    davinci: Switch to QTI bt stack
    Change-Id: I47016b066ae800d0071dbad6a1235cbc9db3cf80
    Signed-off-by: Arian <arian.kulmer@web.de>
    Signed-off-by: maade <p1nchp0d@bk.ru>

--- sm6150.mk
+++ sm6150.mk
@@ -79,7 +79,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    BluetoothQti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
